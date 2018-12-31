4077714cce205123384f3ea9c98f95d87c62dc9d
Szarka <N017M24@chubb.com>
2018-12-31 15:56:38 -0500

    Adding Firestore

--- angular-app/src/angular-examples/angular-examples-routing.module.ts
+++ angular-app/src/angular-examples/angular-examples-routing.module.ts
@@ -1,10 +1,12 @@
+import { FirestoreComponent } from './components/firestore/firestore.component';
 import { AboutComponent } from './../angular-examples/components/about/about.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', children: [
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'firestore', component: FirestoreComponent },
   ]}
 ];
 
--- angular-app/src/angular-examples/angular-examples.module.ts
+++ angular-app/src/angular-examples/angular-examples.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './components/about/about.component';
 import { AngularExamplesRoutingModule } from './angular-examples-routing.module';
+import { FirestoreComponent } from './components/firestore/firestore.component';
 
 @NgModule({
   declarations: 
-  [ AboutComponent ],
+  [ AboutComponent, FirestoreComponent ],
   imports: [
     CommonModule,
     AngularExamplesRoutingModule
